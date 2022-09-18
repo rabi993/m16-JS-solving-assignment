cf22532087cd893bc8fb420622a4b34238c5ca1d
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-18 23:56:48 +0600

    brick calculation

---
+++ brickCalculator.js
@@ -0,0 +1,16 @@
+function brickCalculator(brkPerFeet,kotoTola){
+if(kotoTola < 11){
+    totalBrick =kotoTola*15*brkPerFeet;
+}
+if(kotoTola < 21){
+    totalBrick =(((kotoTola-10)*12*brkPerFeet)+(10*15*brkPerFeet));
+}
+else{
+    totalBrick =(((kotoTola-20)*10*brkPerFeet)+(10*12*brkPerFeet)+(10*15*brkPerFeet));
+}
+return totalBrick;
+}
+var brickPerFeet=1000;
+var kotoTolaBuilding = 21;
+var result = brickCalculator(brickPerFeet , kotoTolaBuilding);
+console.log(result);
